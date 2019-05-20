f25f2a1935956675dcddca78fa3bc707405cf448
vsaryanidi <50796764+vsaryanidi@users.noreply.github.com>
2019-05-20 22:00:41 +0400

    Добавлены новые функции

--- sandbox/src/main/java/ru/sar/neo/MyFirstProgram.java
+++ sandbox/src/main/java/ru/sar/neo/MyFirstProgram.java
@@ -3,7 +3,28 @@ package ru.sar.neo;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-	System.out.println("Hello, world!");
+	  hello("Lera");
+	  hello("world");
+	  hello("Amigo");
+
+	  double l = 5;
+    System.out.println("Площадь квадрата: " + area(l));
+
+    double a = 5.0;
+    double b = 6.0;
+    System.out.println("Площадь прямоугольника: " + area(a,b));
       }
 
+      public static void hello(String s) {
+        System.out.println("Hello, " + s + "!");
+
+      }
+
+      public static double area (double l){
+        return l*l;
+      }
+
+      public static double area (double a, double b){
+        return a*b;
+      }
 }
