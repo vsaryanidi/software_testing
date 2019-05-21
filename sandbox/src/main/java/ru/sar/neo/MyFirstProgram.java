eb26851fc46347189de1c1bb4b907a0ea6d9a5bb
vsaryanidi <50796764+vsaryanidi@users.noreply.github.com>
2019-05-21 21:52:53 +0400

    Результаты решения задания №2 (Вариант 2)

--- sandbox/src/main/java/ru/sar/neo/MyFirstProgram.java
+++ sandbox/src/main/java/ru/sar/neo/MyFirstProgram.java
@@ -14,12 +14,20 @@ public class MyFirstProgram {
     Rectangle r = new Rectangle(4, 6);
     System.out.println("Площадь прямоугольника: " + r.area());
 
+
+    //Задание №2
     Point p1 = new Point(1, 2);
     Point p2 = new Point(5, 5);
 
-    System.out.println("Расстояние между точками p1 и p2 " + "= " + p1.distance(p2));
+    System.out.println("Расстояние между точками p1 и p2 " + "= " + distance(p1,p2));
 
   }
+
+    public static double distance(Point p1,Point p2) {
+      return Math.sqrt((p2.x-p1.x)*(p2.x-p1.x) + (p2.y-p1.y)*(p2.y-p1.y));
+    }
+
+
 
   public static void hello(String s) {
     System.out.println("Hello, " + s + "!");
--- sandbox/src/main/java/ru/sar/neo/Point.java
+++ sandbox/src/main/java/ru/sar/neo/Point.java
@@ -13,9 +13,4 @@ public class Point {
 
   }
 
-  public double distance(Point p2) {
-    return Math.sqrt((p2.x-this.x)*(p2.x-this.x) + (p2.y-this.y)*(p2.y-this.y));
-  }
-
-
 }
