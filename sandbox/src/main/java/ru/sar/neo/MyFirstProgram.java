3cca7bd9a8a2cfb76df73bd181782c8c3752b260
vsaryanidi <50796764+vsaryanidi@users.noreply.github.com>
2019-05-21 21:37:54 +0400

    Результаты решения задания №2 (вариант 1)

--- sandbox/src/main/java/ru/sar/neo/MyFirstProgram.java
+++ sandbox/src/main/java/ru/sar/neo/MyFirstProgram.java
@@ -3,25 +3,28 @@ package ru.sar.neo;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-	  hello("Lera");
-	  hello("world");
-	  hello("Amigo");
+    hello("Lera");
+    hello("world");
+    hello("Amigo");
 
-
-	  Square s = new Square(5);
+    Square s = new Square(5);
     System.out.println("Площадь квадрата: " + s.area());
 
 
-    Rectangle r = new Rectangle(4,6);
+    Rectangle r = new Rectangle(4, 6);
     System.out.println("Площадь прямоугольника: " + r.area());
-      }
 
-      public static void hello(String s) {
-        System.out.println("Hello, " + s + "!");
+    Point p1 = new Point(1, 2);
+    Point p2 = new Point(5, 5);
 
-      }
+    System.out.println("Расстояние между точками p1 и p2 " + "= " + p1.distance(p2));
 
+  }
 
+  public static void hello(String s) {
+    System.out.println("Hello, " + s + "!");
+
+  }
 
 
 }
---
+++ sandbox/src/main/java/ru/sar/neo/Point.java
@@ -0,0 +1,21 @@
+package ru.sar.neo;
+
+public class Point {
+
+  public double x;
+  public double y;
+
+
+  public Point (double x, double y) {
+
+    this.x = x;
+    this.y = y;
+
+  }
+
+  public double distance(Point p2) {
+    return Math.sqrt((p2.x-this.x)*(p2.x-this.x) + (p2.y-this.y)*(p2.y-this.y));
+  }
+
+
+}
