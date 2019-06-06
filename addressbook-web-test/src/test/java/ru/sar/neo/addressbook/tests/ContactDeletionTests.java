fc0e01a0b53f800da373773e63cb4cd6d5689564
vsaryanidi <50796764+vsaryanidi@users.noreply.github.com>
2019-06-07 00:47:34 +0400

    тест

--- addressbook-web-test/src/test/java/ru/sar/neo/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-test/src/test/java/ru/sar/neo/addressbook/tests/ContactDeletionTests.java
@@ -8,67 +8,15 @@ import java.util.concurrent.TimeUnit;
 
 import static org.testng.Assert.assertTrue;
 
-public class ContactDeletionTests {
-  private WebDriver wd;
-  private boolean acceptNextAlert = true;
-
-  @BeforeMethod(alwaysRun = true)
-  public void setUp() throws Exception {
-    wd = new FirefoxDriver();
-    wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
+public class ContactDeletionTests extends TestBase{
 
   @Test
   public void ContactDeletionTests() throws Exception {
-    wd.get("http://localhost/addressbook/");
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys("admin");
-    wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys("secret");
-    wd.findElement(By.xpath("//input[@value='Login']")).click();
-    wd.findElement(By.name("selected[]")).click();
+    selectContact();
     acceptNextAlert = true;
-    wd.findElement(By.xpath("//input[@value='Delete']")).click();
-    assertTrue(closeAlertAndGetItsText().matches("^Delete 1 addresses[\\s\\S]$"));
-    wd.findElement(By.linkText("home")).click();
-    wd.findElement(By.linkText("Logout")).click();
+    deleteSelectedContact();
+    confirmDeletionContact();
+    app.getGroupHelper().returnToGroupPage(); /*по аналогии*/
   }
 
-  @AfterMethod(alwaysRun = true)
-  public void tearDown() throws Exception {
-    wd.quit();
-  }
-
-  private boolean isElementPresent(By by) {
-    try {
-      wd.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
-  }
-
-  private boolean isAlertPresent() {
-    try {
-      wd.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
-  }
-
-  private String closeAlertAndGetItsText() {
-    try {
-      Alert alert = wd.switchTo().alert();
-      String alertText = alert.getText();
-      if (acceptNextAlert) {
-        alert.accept();
-      } else {
-        alert.dismiss();
-      }
-      return alertText;
-    } finally {
-      acceptNextAlert = true;
-    }
-  }
 }
--- addressbook-web-test/src/test/java/ru/sar/neo/addressbook/tests/TestBase.java
+++ addressbook-web-test/src/test/java/ru/sar/neo/addressbook/tests/TestBase.java
@@ -1,12 +1,20 @@
 package ru.sar.neo.addressbook.tests;
 
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.sar.neo.addressbook.appmanager.ApplicationManager;
 
+import java.util.concurrent.TimeUnit;
+
+import static org.testng.Assert.assertTrue;
+
 public class TestBase {
 
   public final ApplicationManager app = new ApplicationManager();
+  public WebDriver wd;
+  public boolean acceptNextAlert = true;
 
   @BeforeMethod(alwaysRun = true)
   public void setUp() throws Exception {
@@ -18,4 +26,48 @@ public class TestBase {
     app.stop();
   }
 
+  private boolean isElementPresent(By by) {
+    try {
+      wd.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      wd.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  protected String closeAlertAndGetItsText() {
+    try {
+      Alert alert = wd.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+
+  protected void confirmDeletionContact() {
+    assertTrue(closeAlertAndGetItsText().matches("^Delete 1 addresses[\\s\\S]$"));
+  }
+
+  protected void deleteSelectedContact() {
+    wd.findElement(By.xpath("//input[@value='Delete']")).click();
+  }
+
+  protected void selectContact() {
+    wd.findElement(By.name("selected[]")).click();
+  }
 }
