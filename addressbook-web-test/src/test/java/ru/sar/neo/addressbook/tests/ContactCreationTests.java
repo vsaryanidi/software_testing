3be3c9ae02faed58b06dd4606e50461ebd994472
vsaryanidi <50796764+vsaryanidi@users.noreply.github.com>
2019-07-19 23:21:02 +0400

    Лекция 6.4. Параметризация тестовых методов

--- addressbook-web-test/src/test/java/ru/sar/neo/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-test/src/test/java/ru/sar/neo/addressbook/tests/ContactCreationTests.java
@@ -5,19 +5,34 @@ import ru.sar.neo.addressbook.model.ContactData;
 import ru.sar.neo.addressbook.model.Contacts;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.CoreMatchers.equalTo;
 
-public class ContactCreationTests extends TestBase{
+public class ContactCreationTests extends TestBase {
 
-  @Test
-  public void testContactCreation()  {
+  @DataProvider
+  public Iterator<Object[]> validContacts() {
+    List<Object[]> list = new ArrayList<Object[]>();
+    List<File> photo = new ArrayList<File>();
+    list.add(new Object[] {new ContactData().withFirstname("Valerie").withLastname("Saryanidi")
+            .withAddress("Germany, Munich").withHome_phone("+7 987 333 33 33")
+            .withEmail("vsaryanidi@gmail.com").withGroup("TestGroup").withPhoto(new File("src/test/resources/photo_2019-07-17_20-05-36.jpg"))});
+    list.add(new Object[] {new ContactData().withFirstname("Valerie").withLastname("Saryanidi")
+            .withAddress("Germany, Munich1").withHome_phone("+7 987 333 33 88")
+            .withEmail("vsaryanidi1@gmail.com").withGroup("TestGroup1").withPhoto(new File("src/test/resources/photo_2019-07-17_20-05-36.jpg"))});
+    return list.iterator();
+  }
+
+  @Test (dataProvider = "validContacts")
+  public void testContactCreation(ContactData contact) throws Exception {
     app.contact().goToHomePage();
     Contacts before = app.contact().all();
     app.contact().gotoContactAddPage();
     File photo = new File("src/test/resources/photo_2019-07-17_20-05-36.jpg");
-    ContactData contact = new ContactData().withFirstname("Valerie").withLastname("Saryanidi").withAddress("Germany, Munich").withHome_phone("+7 987 333 33 33").withEmail("vsaryanidi@gmail.com").withGroup("TestGroup").withPhoto(photo);
     app.contact().fillContactForm(contact,true);
     app.contact().submitContactCreation();
     app.contact().goToHomePage();
--- addressbook-web-test/src/test/java/ru/sar/neo/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-test/src/test/java/ru/sar/neo/addressbook/tests/GroupCreationTests.java
@@ -4,16 +4,28 @@ import org.testng.annotations.*;
 import ru.sar.neo.addressbook.model.GroupData;
 import ru.sar.neo.addressbook.model.Groups;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
 
-  @Test
-  public void testGroupCreation() {
+  @DataProvider
+  public Iterator<Object[]> validGroups (){
+    List<Object[]> list= new ArrayList<Object[]>();
+    list.add(new Object[] {new GroupData().withName("name 1").withHeader("header 1").withFooter("footer 1")});
+    list.add(new Object[] {new GroupData().withName("name 2").withHeader("header 2").withFooter("footer 2")});
+    list.add(new Object[] {new GroupData().withName("name 3").withHeader("header 3").withFooter("footer 3")});
+    return list.iterator();
+  }
+
+  @Test (dataProvider = "validGroups")
+  public void testGroupCreation(GroupData group) throws Exception {
     app.goTo().groupPage();
     Groups before = app.group().all();
-    GroupData group = new GroupData().withName("TestGroup");
     app.group().create(group);
     assertThat(app.group().count(), equalTo(before.size() + 1));
     Groups after = app.group().all();
