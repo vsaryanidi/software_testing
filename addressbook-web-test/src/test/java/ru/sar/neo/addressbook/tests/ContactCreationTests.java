416fe4986cdd7bc9676054ec7c1c8509d90615d1
vsaryanidi <50796764+vsaryanidi@users.noreply.github.com>
2019-07-20 16:22:25 +0400

    Лекция 6.5. Загрузка тестовых данных из файла для контактов

--- addressbook-web-test/src/test/java/ru/sar/neo/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-test/src/test/java/ru/sar/neo/addressbook/tests/ContactCreationTests.java
@@ -4,7 +4,10 @@ import org.testng.annotations.*;
 import ru.sar.neo.addressbook.model.ContactData;
 import ru.sar.neo.addressbook.model.Contacts;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -15,24 +18,36 @@ import static org.hamcrest.CoreMatchers.equalTo;
 public class ContactCreationTests extends TestBase {
 
   @DataProvider
-  public Iterator<Object[]> validContacts() {
+
+  public Iterator<Object[]> validContacts() throws IOException {
     List<Object[]> list = new ArrayList<Object[]>();
-    List<File> photo = new ArrayList<File>();
-    list.add(new Object[] {new ContactData().withFirstname("Valerie").withLastname("Saryanidi")
-            .withAddress("Germany, Munich").withHome_phone("+7 987 333 33 33")
-            .withEmail("vsaryanidi@gmail.com").withGroup("TestGroup").withPhoto(new File("src/test/resources/photo_2019-07-17_20-05-36.jpg"))});
-    list.add(new Object[] {new ContactData().withFirstname("Valerie").withLastname("Saryanidi")
-            .withAddress("Germany, Munich1").withHome_phone("+7 987 333 33 88")
-            .withEmail("vsaryanidi1@gmail.com").withGroup("TestGroup1").withPhoto(new File("src/test/resources/photo_2019-07-17_20-05-36.jpg"))});
+    //List<File> photo = new ArrayList<File>();
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.csv")));
+    String line = reader.readLine();
+    while (line != null) {
+      String[] split = line.split(";");
+      list.add(new Object[]{(new ContactData()
+              .withFirstname(split[0])
+              .withLastname(split[1])
+              .withHome_phone(split[2])
+              .withMobile_phone(split[3])
+              .withWork_phone(split[4])
+              .withAddress(split[5])
+              .withEmail(split[6])
+              .withEmail1(split[7])
+              .withEmail2(split[8])
+              .withGroup(split[9]))});
+      line = reader.readLine();
+    }
     return list.iterator();
   }
 
   @Test (dataProvider = "validContacts")
+
   public void testContactCreation(ContactData contact) throws Exception {
     app.contact().goToHomePage();
     Contacts before = app.contact().all();
     app.contact().gotoContactAddPage();
-    File photo = new File("src/test/resources/photo_2019-07-17_20-05-36.jpg");
     app.contact().fillContactForm(contact,true);
     app.contact().submitContactCreation();
     app.contact().goToHomePage();
