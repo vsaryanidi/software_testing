ee52a3183497d259188c9d5f307ef92acb6f71f6
vsaryanidi <50796764+vsaryanidi@users.noreply.github.com>
2019-07-20 22:03:51 +0400

    Лекция 6.11. Протоколирование (лог-файлы)

--- addressbook-web-test/src/test/java/ru/sar/neo/addressbook/tests/TestBase.java
+++ addressbook-web-test/src/test/java/ru/sar/neo/addressbook/tests/TestBase.java
@@ -1,5 +1,7 @@
 package ru.sar.neo.addressbook.tests;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
@@ -7,9 +9,14 @@ import org.testng.annotations.BeforeSuite;
 import ru.sar.neo.addressbook.appmanager.ApplicationManager;
 import org.openqa.selenium.remote.BrowserType;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 import static org.testng.Assert.assertTrue;
 
 public class TestBase{
+
+  Logger logger = LoggerFactory.getLogger(TestBase.class);
 
   protected static final ApplicationManager app
           = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
@@ -24,4 +31,13 @@ public class TestBase{
   app.stop();
   }
 
+  @BeforeMethod (alwaysRun = true)
+  public void logTestStart (Method m, Object [] p) {
+    logger.info("Start test " + m.getName() + " with parameters " + Arrays.asList(p));
+  }
+
+  @AfterMethod (alwaysRun = true)
+  public void logTestStop (Method m) {
+    logger.info("Stop test " + m.getName());
+  }
 }
