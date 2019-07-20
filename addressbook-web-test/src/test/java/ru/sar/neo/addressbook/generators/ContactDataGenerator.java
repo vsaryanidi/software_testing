c97e8c97884936d205f547af92e6ded532eee258
vsaryanidi <50796764+vsaryanidi@users.noreply.github.com>
2019-07-20 14:49:26 +0400

    update

--- addressbook-web-test/src/test/java/ru/sar/neo/addressbook/generators/ContactDataGenerator.java
+++ addressbook-web-test/src/test/java/ru/sar/neo/addressbook/generators/ContactDataGenerator.java
@@ -37,28 +37,28 @@ public class ContactDataGenerator {
     save(contacts, new File(file));
   }
 
-    private void save(List<ContactData> contacts, File file) throws IOException {
-      Writer writer = new FileWriter(file);
-      for (ContactData contact : contacts){
-        writer.write(String.format("%s;%s;%s;%s;%s;%s;%s;%s;%s;%s\n", contact.getFirstname(), contact.getLastname(),
-                contact.getHome_phone(), contact.getMobile_phone(),contact.getWork_phone(),contact.getAddress(),
-                contact.getEmail(),contact.getEmail1(),contact.getEmail2(),contact.getGroup()));
-      }
-      writer.close();
+  private void save(List<ContactData> contacts, File file) throws IOException {
+    Writer writer = new FileWriter(file);
+    for (ContactData contact : contacts){
+      writer.write(String.format("%s;%s;%s;%s;%s;%s;%s;%s;%s;%s\n", contact.getFirstname(), contact.getLastname(),
+              contact.getHome_phone(), contact.getMobile_phone(),contact.getWork_phone(),contact.getAddress(),
+              contact.getEmail(),contact.getEmail1(),contact.getEmail2(),contact.getGroup()));
     }
+    writer.close();
+  }
 
-    private List<ContactData> generateContacts (int count) {
-      List<ContactData> contacts = new ArrayList<ContactData>();
-      for (int i = 0; i < count; i++) {
-        contacts.add(new ContactData()
-                .withFirstname(String.format("Firstname %s",i)).withLastname(String.format("Lastname %s",i))
-                .withHome_phone(String.format("Home_phone %s",i)).withMobile_phone(String.format("Mobile_phone %s",i))
-                .withWork_phone(String.format("Work_phone %s",i))
-                .withAddress(String.format("Address %s",i)).withEmail(String.format("Email %s",i))
-                .withEmail1(String.format("Email1 %s",i)).withEmail2(String.format("Email2 %s",i))
-                .withGroup(String.format("Group %s",i)));
-      }
-      return contacts;
+  private List<ContactData> generateContacts (int count) {
+    List<ContactData> contacts = new ArrayList<ContactData>();
+    for (int i = 0; i < count; i++) {
+      contacts.add(new ContactData()
+              .withFirstname(String.format("Firstname %s",i)).withLastname(String.format("Lastname %s",i))
+              .withHome_phone(String.format("Home_phone %s",i)).withMobile_phone(String.format("Mobile_phone %s",i))
+              .withWork_phone(String.format("Work_phone %s",i))
+              .withAddress(String.format("Address %s",i)).withEmail(String.format("Email %s",i))
+              .withEmail1(String.format("Email1 %s",i)).withEmail2(String.format("Email2 %s",i))
+              .withGroup(String.format("Group %s",i)));
     }
+    return contacts;
+  }
 
 }
--- addressbook-web-test/src/test/java/ru/sar/neo/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-test/src/test/java/ru/sar/neo/addressbook/generators/GroupDataGenerator.java
@@ -39,7 +39,7 @@ public class GroupDataGenerator {
   }
 
   private void save(List<GroupData> groups, File file) throws IOException {
-  Writer writer = new FileWriter(file);
+    Writer writer = new FileWriter(file);
     for (GroupData group : groups){
       writer.write(String.format("%s;%s;%s\n", group.getName(), group.getHeader(), group.getFooter()));
     }
@@ -49,10 +49,9 @@ public class GroupDataGenerator {
   private List<GroupData> generateGroups(int count) {
     List<GroupData> groups = new ArrayList<GroupData>();
     for (int i = 0; i < count; i++) {
-      groups.add(new GroupData().withName(String.format("test %s",i))
+      groups.add(new GroupData().withName(String.format("Group %s",i))
               .withHeader(String.format("header %s",i)).withFooter(String.format("footer %s",i)));
     }
     return groups;
   }
 }
-
