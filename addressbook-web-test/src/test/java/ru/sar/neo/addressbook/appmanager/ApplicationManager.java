1146e7ad11a21f6f8947c0285155e8b120c9cc1d
vsaryanidi <50796764+vsaryanidi@users.noreply.github.com>
2019-07-20 21:24:53 +0400

    Лекция 6.10. Описание конфигурации тестового стенда

--- addressbook-web-test/src/test/java/ru/sar/neo/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-test/src/test/java/ru/sar/neo/addressbook/appmanager/ApplicationManager.java
@@ -8,6 +8,11 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager{
@@ -20,11 +25,17 @@ public class ApplicationManager{
   public GroupHelper groupHelper;
   private String browser;
 
+  private final Properties properties;
   public ApplicationManager(String browser) {
     this.browser = browser;
+    properties = new Properties();
   }
 
-  public void init() {
+  public void init() throws IOException {
+
+    String target = System.getProperty("target", "local");
+    properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
     if (browser.equals(BrowserType.FIREFOX)) {
       wd = new FirefoxDriver();
     }
@@ -36,12 +47,12 @@ public class ApplicationManager{
     }
 
     wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-    wd.get("http://localhost/addressbook/group.php");
+    wd.get(properties.getProperty("web.baseURL"));
     contactHelper = new ContactHelper(wd);
     groupHelper = new GroupHelper(wd);
     navigationHelper = new NavigationHelper(wd);
     sessionHelper = new SessionHelper(wd);
-    sessionHelper.login("admin", "secret");
+    sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
   }
 
   public void stop() {
