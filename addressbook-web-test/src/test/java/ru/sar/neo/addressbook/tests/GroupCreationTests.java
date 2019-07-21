48e48c35e2825d7980d9bbb201bde1968a9f96d4
vsaryanidi <50796764+vsaryanidi@users.noreply.github.com>
2019-07-21 11:43:25 +0400

    Лекция 7.4. Тест на создание группы

--- addressbook-web-test/src/test/java/ru/sar/neo/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-test/src/test/java/ru/sar/neo/addressbook/tests/GroupCreationTests.java
@@ -38,10 +38,10 @@ public class GroupCreationTests extends TestBase {
   @Test (dataProvider = "validGroups")
   public void testGroupCreation(GroupData group) throws Exception {
     app.goTo().groupPage();
-    Groups before = app.group().all();
+    Groups before = app.db().groups();
     app.group().create(group);
     assertThat(app.group().count(), equalTo(before.size() + 1));
-    Groups after = app.group().all();
+    Groups after = app.db().groups();
     assertThat(after, equalTo(
             before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
 
@@ -50,11 +50,11 @@ public class GroupCreationTests extends TestBase {
   @Test
   public void testBadGroupCreation() {
     app.goTo().groupPage();
-    Groups before = app.group().all();
+    Groups before = app.db().groups();
     GroupData group = new GroupData().withName("TestGroup'");
     app.group().create(group);
     assertThat(app.group().count(), equalTo(before.size()));
-    Groups after = app.group().all();
+    Groups after = app.db().groups();
     assertThat(after, equalTo(before));
 
   }
